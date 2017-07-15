4ac396fa78015e09e1fc2bd583c089fc45def719
Sanyam Agrawal <sanyam4u@gmail.com>
2017-07-15 14:45:13 +0530

    create bundle.js

--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
+import './styles/main.css';
+
 console.log('Hello from Webpack');
 
 class Name {
--- tools/webpack/webpack.config.dev.js
+++ tools/webpack/webpack.config.dev.js
@@ -1,14 +1,26 @@
 const path = require('path');
-
-const OUTPUT_DIRECTORY = path.join(__dirname, '../../dist');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const OUTPUT_DIRECTORY = path.join(process.cwd(), '/dist');
+console.log('OUTPUT_DIRECTORY', OUTPUT_DIRECTORY);
 const webpackDevConfig = {
   entry: './client/index.js',
   output: {
-    path: OUTPUT_DIRECTORY,
-    filename: 'bundle.js'
+    path: `${OUTPUT_DIRECTORY}/assets`,
+    filename: 'bundle.js',
+    publicPath: '/assets/'
   },
-  watch: true
+  watch: true,
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ["style-loader","css-loader"]
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: `${OUTPUT_DIRECTORY}/index.html`
+    })
+  ]
 };
 
 module.exports = webpackDevConfig;
